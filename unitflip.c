7c4bd87f7957ec2b069c3ef0b2633d71b44ad474
Gh0st-0f-Th <work.rasbiharisingha@gmail.com>
2026-05-29 03:41:07 +0600

    Update prompt for unit conversion input

--- unitflip.c
+++ unitflip.c
@@ -5,7 +5,7 @@ int main() {
     printf("\nTo convert your unit to your target unit, enter how many \ntarget units equal to 1 of the unit you want to convert from.\n\nFor example:\n   USD to Euro  : 1 USD = 0.86 EUR, enter 0.86\n   KM to Miles  : 1 KM  = 0.62 MI,  enter 0.62\n   KG to Pounds : 1 KG  = 2.20 LBS, enter 2.20\n\n   Otherwise, the result will be incorrect!\n\nEnter any non-numerical key to terminate the program!\n\n=> ");
     for(;;) {
     if (scanf("%lf",&ratio)) {
-        printf("=> Now, enter the currency you want to convert from.\n");
+        printf("=> Now, enter the unit you want to convert from.\n");
         for (;;) {
             printf("=> ");
             if (scanf("%lf",&unit)) {
