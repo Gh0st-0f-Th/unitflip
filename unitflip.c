69f0a93f9292fbcde4bbfd8019a1b71ddf921c2e
Gh0st-0f-Th <work.rasbiharisingha@gmail.com>
2026-05-29 19:04:54 +0600

    Removed unnecessary outer loop

--- unitflip.c
+++ unitflip.c
@@ -3,7 +3,6 @@ double ratio,unit;
 
 int main() {
     printf("\nTo convert your unit to your target unit, enter how many \ntarget units equal to 1 of the unit you want to convert from.\n\nFor example:\n   USD to Euro  : 1 USD = 0.86 EUR, enter 0.86\n   KM to Miles  : 1 KM  = 0.62 MI,  enter 0.62\n   KG to Pounds : 1 KG  = 2.20 LBS, enter 2.20\n\n   Otherwise, the result will be incorrect!\n\nEnter any non-numerical key to terminate the program!\n\n=> ");
-    for(;;) {
     if (scanf("%lf",&ratio)>0) {
         printf("=> Now, enter the unit you want to convert from.\n");
         for (;;) {
@@ -22,6 +21,4 @@ int main() {
         }
         
     } else {printf("Invalid input! Exiting...\n"); return 0; }
-    }
-    return 0;
 }
