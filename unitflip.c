2e0c9f3856038e37c97415f69fc2c30e8f83e55f
Rasbihari Singha <work.rasbiharisingha@gmail.com>
2026-05-29 20:00:49 +0600

    Fix variable declaration for ratio and unit

--- unitflip.c
+++ unitflip.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-double ratio,unit;
 
 int main() {
+    double ratio,unit;
     printf("\nTo convert your unit to your target unit, enter how many \ntarget units equal to 1 of the unit you want to convert from.\n\nFor example:\n   USD to Euro  : 1 USD = 0.86 EUR, enter 0.86\n   KM to Miles  : 1 KM  = 0.62 MI,  enter 0.62\n   KG to Pounds : 1 KG  = 2.20 LBS, enter 2.20\n\n   Otherwise, the result will be incorrect!\n\nEnter any non-numerical key to terminate the program!\n\n=> ");
     if (scanf("%lf",&ratio)>0) {
         printf("=> Now, enter the unit you want to convert from.\n");
