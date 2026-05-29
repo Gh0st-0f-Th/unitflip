474dce3e12a98e557b141863954e914c28989c13
Gh0st-0f-Th <work.rasbiharisingha@gmail.com>
2026-05-29 18:36:09 +0600

    Fix scanf checks for input validation

--- unitflip.c
+++ unitflip.c
@@ -4,11 +4,11 @@ double ratio,unit;
 int main() {
     printf("\nTo convert your unit to your target unit, enter how many \ntarget units equal to 1 of the unit you want to convert from.\n\nFor example:\n   USD to Euro  : 1 USD = 0.86 EUR, enter 0.86\n   KM to Miles  : 1 KM  = 0.62 MI,  enter 0.62\n   KG to Pounds : 1 KG  = 2.20 LBS, enter 2.20\n\n   Otherwise, the result will be incorrect!\n\nEnter any non-numerical key to terminate the program!\n\n=> ");
     for(;;) {
-    if (scanf("%lf",&ratio)) {
+    if (scanf("%lf",&ratio)>0) {
         printf("=> Now, enter the unit you want to convert from.\n");
         for (;;) {
             printf("=> ");
-            if (scanf("%lf",&unit)) {
+            if (scanf("%lf",&unit)>0) {
                 double result=unit*ratio;
                 int round=(int)result;
                 if (round==result) {
